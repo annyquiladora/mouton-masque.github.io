6ccec0a5677392e262702dbdbc22633925738fb9
annyquiladora <anny_barrero@hotmail.com>
2023-06-19 22:03:10 +0200

    mdification comment code

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-// import { User } from '../../models/user.model';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
@@ -13,15 +12,5 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  // getUser(user: User): Observable<any> {
-  //   const headers = new HttpHeaders({ 'x-access-token': user.token });
-  //   return this.http.get(this.userUrl + user._id, { headers: headers });
-  // }
-
-  // updateUser(user: User): Observable<any> {
-  //   const headers = new HttpHeaders({ 'x-access-token': user.token });
-  //   return this.http.put(this.userUrl + user._id, user, { headers: headers });
-  // }
-
-
+  
 }
